cc54cf79a9fc76f6709d941f40374b718e40ff1f
IDSAD <idsad.ch@gmail.com>
2021-06-09 00:15:47 +0200

    many changes and fixes after review

--- setters-test-work/js/common.min.js
+++ setters-test-work/js/common.min.js
@@ -1,8 +1,4 @@
-const scrollWrapper = document.querySelector('#scrollWrapper')
-const scrollCircle = document.querySelector('#scrollCircle')
-const scrollArrow = document.querySelector('#scrollArrow')
-
-let magnetScroll = new MagnetMouse({
+const magnetScroll = new MagnetMouse({
 	magnet: {
 		element: '#scrollWrapper',
 		position: 'center',
