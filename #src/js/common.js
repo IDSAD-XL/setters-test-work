de8a1c3a9ac34af016877036d6a49ae971babe9b
IDSAD <idsad.ch@gmail.com>
2021-06-24 21:28:19 +0200

    refactored structure

--- #src/js/common.js
+++ #src/js/common.js
@@ -81,23 +81,28 @@ class hoverEffect {
 }
 const magnetScroll = document.querySelector("#scrollWrapper");
 const contentSection = document.querySelector('.content')
-new hoverEffect(magnetScroll);
-magnetScroll.addEventListener('click', (e) => {
-	e.preventDefault()
-	contentSection.scrollIntoView({ behavior: "smooth" })
-})
+if (magnetScroll && contentSection) {
+	new hoverEffect(magnetScroll);
+	magnetScroll.addEventListener('click', (e) => {
+		e.preventDefault()
+		contentSection.scrollIntoView({ behavior: "smooth" })
+	})
+}
+
 
 //! Mobile burger menu
 const menuCheckbox = document.querySelector('#menu__toggle')
 const body = document.querySelector('body');
 
-menuCheckbox.addEventListener('change', () => {
-	if (menuCheckbox.checked) {
-		setBodyScrollable()
-	} else {
-		setBodyUnScrollable()
-	}
-})
+if (menuCheckbox) {
+	menuCheckbox.addEventListener('change', () => {
+		if (menuCheckbox.checked) {
+			setBodyScrollable()
+		} else {
+			setBodyUnScrollable()
+		}
+	})
+}
 
 function setBodyScrollable() {
 	body.classList.add('menu-open')
@@ -110,11 +115,16 @@ function setBodyUnScrollable() {
 //! Footer scroll to top arrows
 const scrollTopArrows = document.querySelectorAll(".footer-sticky__arrow-top")
 const topSection = document.querySelector("#top-section")
-scrollTopArrows.forEach((e) => {
-	e.addEventListener('click', () => {
-		topSection.scrollIntoView({ behavior: "smooth" })
+
+if (scrollTopArrows) {
+	scrollTopArrows.forEach((e) => {
+		e.addEventListener('click', () => {
+			// topSection.scrollIntoView({ behavior: "smooth" })
+			body.animate({ scrollTop: 0 }, 800)
+		})
 	})
-})
+
+}
 
 //! Footer hide at top section
 const stickyFooter = document.querySelector("#footer-sticky")
